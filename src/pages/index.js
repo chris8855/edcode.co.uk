8e1aa5c61e1380a1157b60c09a442c80f96cd19b
chris8855 <chris@englishtutorslive.com>
2020-03-30 15:31:42 +0200

    Change text on splash page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hello everyone</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Welcome to edcode.co.uk</h1>
+    <p>This site is currently under develpment</p>
+    <p>Thanks for your interest</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
