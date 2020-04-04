2f1fc8bbdc6c1ec2dc27567770470938c9a3eb31
Richard Moss <richard@englishtutorslive.com>
2020-04-04 18:54:33 +0100

    Hook up video room

--- src/components/video-display.js
+++ src/components/video-display.js
@@ -3,13 +3,21 @@ import useTwilioVideo from "../hooks/useTwilioVideo"
 import { navigate } from "gatsby"
 
 const VideoDisplay = ({ roomID }) => {
-  const { state } = useTwilioVideo()
+  const { state, startVideo, videoRef } = useTwilioVideo()
   useEffect(() => {
     if (!state.token) {
       navigate("/chat", { state: { roomName: roomID } })
     }
-  }, [state, roomID])
-  return <h1>room: {roomID}</h1>
+    if (!state.room) {
+      startVideo()
+    }
+  }, [state, roomID, startVideo])
+  return (
+    <>
+      <div className="chat" ref={videoRef} />
+      <h1>room: {roomID}</h1>
+    </>
+  )
 }
 
 export default VideoDisplay
--- src/hooks/useTwilioVideo.js
+++ src/hooks/useTwilioVideo.js
@@ -1,5 +1,6 @@
-import React, { createContext, useContext, useReducer } from "react"
+import React, { createContext, useContext, useReducer, useRef } from "react"
 import axios from "axios"
+import { connect } from "twilio-video"
 
 const TWILIO_TOKEN_URL =
   "https://salmon-porcupine-5758.twil.io/create-room-token"
@@ -8,6 +9,7 @@ const DEFAULT_STATE = {
   identity: false,
   roomName: false,
   token: false,
+  room: false,
 }
 
 const reducer = (state, action) => {
@@ -19,6 +21,8 @@ const reducer = (state, action) => {
         identity: action.identity,
         roomName: action.roomName,
       }
+    case "set-active-room":
+      return { ...state, room: action.room }
     default:
       return DEFAULT_STATE
   }
@@ -38,6 +42,7 @@ export const wrapRootElement = ({ element }) => (
 
 const useTwilioVideo = () => {
   const [state, dispatch] = useContext(TwilioVideoContext)
+  const videoRef = useRef()
   const getRoomToken = async ({ identity, roomName }) => {
     const result = await axios.post(TWILIO_TOKEN_URL, {
       identity,
@@ -45,7 +50,33 @@ const useTwilioVideo = () => {
     })
     dispatch({ type: "join", token: result.data, identity, roomName })
   }
-  return { state, getRoomToken }
+
+  const connectToRoom = async () => {
+    if (!state.token) {
+      return
+    }
+
+    const room = await connect(state.token, {
+      name: state.roomName,
+      audio: true,
+      video: { width: 640 },
+      logLevel: "info",
+    }).catch(error => {
+      console.error(`unable to join: ${error}`)
+    })
+
+    const localTrack = [...room.localParticipant.videoTracks.values()][0].track
+
+    if (!videoRef.current.hasChildNodes()) {
+      const localEl = localTrack.attach()
+      videoRef.current.appendChild(localEl)
+    }
+    dispatch({ type: "set-active-room", room })
+  }
+
+  const startVideo = () => connectToRoom()
+
+  return { state, getRoomToken, startVideo, videoRef }
 }
 
 export default useTwilioVideo
